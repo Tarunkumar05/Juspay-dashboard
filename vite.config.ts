833a220ae09e3f13d6ac752bcdfd0a8319a3876e
Tarunkumar05 <101545470+Tarunkumar05@users.noreply.github.com>
2025-09-23 21:34:55 +0530

    dependancy and path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import path from 'path'
 
 export default defineConfig({
   plugins: [
     react(), 
-    svgr()
+    svgr({
+      svgrOptions: {
+        exportType: 'default',
+      },
+    })
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   css: {
     preprocessorOptions: {
       scss: {
         additionalData: `
-          @import "/src/styles/globals/variables.scss";
-          @import "/src/styles/globals/mixin.scss";
-          @import "/src/styles/globals/typography.scss";
+          @import "@/styles/globals/variables.scss";
+          @import "@/styles/globals/mixin.scss";
+          @import "@/styles/globals/typography.scss";
         `
       }
     }
