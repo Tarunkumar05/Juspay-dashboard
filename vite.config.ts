8741624554a0f1949f073139e95f7c55dac78f38
Tarun Kumar Bairwa <101545470+Tarunkumar05@users.noreply.github.com>
2025-09-23 21:43:26 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,35 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import svgr from 'vite-plugin-svgr'
+// import path from 'path'
+
+// export default defineConfig({
+//   plugins: [
+//     react(), 
+//     svgr({
+//       svgrOptions: {
+//         exportType: 'default',
+//       },
+//     })
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src'),
+//     },
+//   },
+//   css: {
+//     preprocessorOptions: {
+//       scss: {
+//         additionalData: `
+//           @import "@/styles/globals/variables.scss";
+//           @import "@/styles/globals/mixin.scss";
+//           @import "@/styles/globals/typography.scss";
+//         `
+//       }
+//     }
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
@@ -10,6 +42,7 @@ export default defineConfig({
       svgrOptions: {
         exportType: 'default',
       },
+      include: '**/*.svg?react',
     })
   ],
   resolve: {
@@ -21,9 +54,9 @@ export default defineConfig({
     preprocessorOptions: {
       scss: {
         additionalData: `
-          @import "@/styles/globals/variables.scss";
-          @import "@/styles/globals/mixin.scss";
-          @import "@/styles/globals/typography.scss";
+          @import "${path.resolve(__dirname, './src/styles/globals/variables.scss')}";
+          @import "${path.resolve(__dirname, './src/styles/globals/mixin.scss')}";
+          @import "${path.resolve(__dirname, './src/styles/globals/typography.scss')}";
         `
       }
     }
