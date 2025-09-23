4b05ccc1a80ba6e3dbaf7704463df3249da08aa5
Tarun Kumar Bairwa <101545470+Tarunkumar05@users.noreply.github.com>
2025-09-23 21:55:20 +0530

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,26 @@
+// /// <reference types="vite-plugin-svgr/client" />
+
+// declare module "*.svg?react" {
+//   import * as React from "react";
+//   const ReactComponent: React.FunctionComponent<
+//     React.SVGProps<SVGSVGElement> & { title?: string }
+//   >;
+//   export default ReactComponent;
+// }
+
+/// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
 
 declare module "*.svg?react" {
-  import * as React from "react";
-  const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & { title?: string }
-  >;
+  import React from "react";
+  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
   export default ReactComponent;
+}
+
+declare module "*.svg" {
+  import React from "react";
+  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  export { ReactComponent };
+  const content: string;
+  export default content;
 }
